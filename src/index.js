b5d1216c7d641bb7aa984807e957cc20e0d503ec
nickbarrow <nickb259@gmail.com>
2021-05-23 03:06:53 -0400

    Install react-snapshot to prerender at build time

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { render } from 'react-snapshot'
 
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/styles.scss";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   rootElement
+// );
+
+render(
+  <App />,
+  document.getElementById('root')
+)
